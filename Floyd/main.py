67e6f2167afa91c0074c10229be1620eb3e4e045
Damir Syzdykov <125348732+ki55el@users.noreply.github.com>
2023-04-12 18:27:47 +0600

    Update main.py

--- Floyd/main.py
+++ Floyd/main.py
@@ -1,7 +1,7 @@
 with open('Input.txt', 'r') as Inp:
     D = [[float(j) for j in i.split()] for i in Inp.read().split('\n')]
-for d in range(len(D)):
+for k in range(len(D)):
     for i in range(len(D)):
         for j in range(len(D)):
-            D[i][j] = min(D[i][d] + D[d][j], D[i][j])
+            D[i][j] = min(D[i][k] + D[k][j], D[i][j])
 [print(*map(int, line)) for line in D]
